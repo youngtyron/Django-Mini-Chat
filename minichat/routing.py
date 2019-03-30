3d18ff25ed4eb7f87789833c4937fe578a5b7178
youngtyron <gro-jorge@mail.ru>
2019-03-30 20:11:30 +0500

    Base set up

---
+++ minichat/routing.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+
+
+application = ProtocolTypeRouter({
+	'websocket': AllowedHostsOriginValidator(
+			AuthMiddlewareStack(
+					URLRouter([
+						# url()
+						])
+				)
+		)
+	})
