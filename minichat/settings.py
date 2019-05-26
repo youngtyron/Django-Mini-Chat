da8910f2236634aab9ce6362b21da6ee9ba7bc8b
youngtyron <gro-jorge@mail.ru>
2019-05-26 20:46:22 +0500

    Nginx and Supervisor conf files

--- minichat/settings.py
+++ minichat/settings.py
@@ -81,36 +81,27 @@ CACHES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'TEST': {
+#             'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+#         }
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'TEST': {
-            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
-        }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chatbase',
+        'USER': 'chatuser',
+        'PASSWORD': 'chatpassword',
+        'HOST':'127.0.0.1',
+        'PORT': 5432,
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME':'postgres',
-#         'USER':'postgres',
-#         'HOST':'db',
-#         'PORT': 5432,
-#     }
-# }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'chatbase',
-#         'USER': 'chatuser',
-#         'PASSWORD': 'chatpassword',
-#         'HOST':'127.0.0.1',
-#         'PORT': '',
-#     }
-# }
 
 
 
